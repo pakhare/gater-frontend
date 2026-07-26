f6aee80d84c2c4953ff4542f6372f49f95064b55
Prasad Khare <prasadkhare261@gmail.com>
2026-07-26 21:24:13 +0530

    Change button container to fixed position

--- src/Question/Question.js
+++ src/Question/Question.js
@@ -108,7 +108,7 @@ export default class Question extends Component {
               ))}
             </div>
 
-            <div className="flex flex-wrap gap-3 pt-2">
+            <div className="fixed bottom-0 left-0 right-0 z-50 flex justify-center gap-3 border-t border-slate-200 bg-white p-4 dark:border-slate-700 dark:bg-slate-900">
               <button
                 className="rounded-full border border-slate-300 bg-white px-5 py-2.5 font-semibold text-slate-700 shadow-sm transition hover:bg-slate-100 focus:outline-none focus:ring-4 focus:ring-slate-500/10 dark:border-slate-700 dark:bg-slate-950 dark:text-slate-200 dark:hover:bg-slate-800"
                 onClick={() => {
